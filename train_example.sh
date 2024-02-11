992c3d14ecdfb14478529fbc918ce7618fe5fa78
hnmr <hnmr293@gmail.com>
2024-02-11 18:34:10 +0900

    rename train_module.sh -> train_example.sh

--- train_example.sh
+++ train_example.sh
@@ -2,14 +2,13 @@
 
 b=4
 r="1e-6"
-#t=M00
 R=2048
 
 for t in IN01 IN02 IN04 IN05 IN07 IN08 M00 OUT03 OUT04 OUT05 OUT06 OUT07 OUT08 OUT09 OUT10 OUT11
 do
 
 python train_module.py \
-    --teacher_model='../models/evbp-anime-half.safetensors' \
+    --teacher_model='mymodel.safetensors' \
     --target="${t}" \
     --batch_size="${b}" \
     --lr="${r}" \
